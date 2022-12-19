7e2dceb0f1c78ee2e9dd04162b8e3bf8980a94dc
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-12-19 15:04:12 -0300

    Feature: Estilizando a página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,36 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="">Vou trabalhar em</label>
+          <input type="text" />
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
