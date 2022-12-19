907a3f0b4334d234f4c146fd50a6446775cf1839
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-12-19 15:30:31 -0300

    Feature: Melhoria nos inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,12 +19,22 @@ export function Home() {
             type="text"
             id="task"
             placeholder="De um nome para o seu projeto"
+            list="task-sugestions"
           />
+          <datalist id="task-sugestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
